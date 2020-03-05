60f6040088af7e57b79534a9740e158975569dae
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-05 14:39:39 +0900

    registration
    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_image, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day,])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
