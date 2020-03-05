76787e496bdd88a5d8645219ba84b3e23511643a
takumitorii <takumitorii@users.noreply.github.com>
2020-03-05 19:51:11 +0900

    routes_fix
    誤字の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
   resources :users
-  resources :produts, only: [:index, :show]
+  resources :products, only: [:index, :show]
 end
