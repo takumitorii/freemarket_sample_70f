b17d0c7b48f7c5332d7822cd5bc9085969229a08
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-05 18:26:01 +0900

    スッキリ！
    エラー解決

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_122732) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "birth_year", null: false
-    t.date "birth_month", null: false
     t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
