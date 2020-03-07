60819713421f58c4404c836ecb8447877a4e306f
takumitorii <takumitorii@users.noreply.github.com>
2020-03-07 14:57:34 +0900

    create.users.show
    users/showのデザインの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
-  resources :users
+  resources :users, only: :show
   resources :products, only: [:index, :new, :show]
 end
