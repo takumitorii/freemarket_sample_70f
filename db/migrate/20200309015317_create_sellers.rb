69751c8b90c40a9ead8374b1c71cbe5ac9589f49
takumitorii <takumitorii@users.noreply.github.com>
2020-03-09 14:36:57 +0900

    product/show.create
    商品詳細ページに商品の情報を表示

--- db/migrate/20200309015317_create_sellers.rb
+++ db/migrate/20200309015317_create_sellers.rb
@@ -2,7 +2,7 @@ class CreateSellers < ActiveRecord::Migration[5.2]
   def change
     create_table :sellers do |t|
       t.integer :user_id, null: false, foreign_key: true
-
+      t.integer :shipping_info_id, null: false, foreign_key: true
       t.timestamps
     end
   end
