5cc6cd0fb8191fdb30dc46f5cd88c6bf16402df0
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-09 11:58:50 +0900

    product_image変更

--- db/migrate/20200309015130_create_product_images.rb
+++ db/migrate/20200309015130_create_product_images.rb
@@ -2,7 +2,6 @@ class CreateProductImages < ActiveRecord::Migration[5.2]
   def change
     create_table :product_images do |t|
       t.string :product_image,   null: false
-      t.integer :product_id,     null: false, foreign_key: true
 
       t.timestamps
     end
