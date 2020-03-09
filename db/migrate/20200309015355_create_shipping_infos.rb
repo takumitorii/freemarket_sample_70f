40a165cf032a834fc2a7989fd348c3dd52bac53f
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-09 11:54:46 +0900

    修正

--- db/migrate/20200309015355_create_shipping_infos.rb
+++ db/migrate/20200309015355_create_shipping_infos.rb
@@ -1,6 +1,6 @@
-class CreateShippings < ActiveRecord::Migration[5.2]
+class CreateShipping_infos < ActiveRecord::Migration[5.2]
   def change
-    create_table :shippings do |t|
+    create_table :shipping_infos do |t|
       t.integer :shipping_cost,   null: false
       t.string  :shipping_area,   null: false
       t.integer :shipping_days,   null: false
