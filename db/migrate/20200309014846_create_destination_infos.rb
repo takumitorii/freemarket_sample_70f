b5761d1ca18fbe7eca67b15962c550b9884f11fb
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-09 12:04:54 +0900

    修正

--- db/migrate/20200309014846_create_destination_infos.rb
+++ db/migrate/20200309014846_create_destination_infos.rb
@@ -1,7 +1,7 @@
 class CreateDestinationInfos < ActiveRecord::Migration[5.2]
   def change
     create_table :destination_infos do |t|
-      t.user_id :integer,           null: false, foreign_key: true
+      t.integer :user_id,           null: false, foreign_key: true
       t.string :family_name,        null: false
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
