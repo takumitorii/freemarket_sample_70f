62759afc1bd7efcfe70faac63fee96d702fa0875
takumitorii <takumitorii@users.noreply.github.com>
2020-03-10 12:30:42 +0900

    fix

--- db/migrate/20200305102432_create_products.rb
+++ db/migrate/20200305102432_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :description,   null: false
       t.string :status,        null: false
       t.integer :category_id,  null: false, foreign_key: true
-      t.integer :brand_id,     null: false, foreign_key: true
+      t.integer :brand_id,     foreign_key: true
       t.integer :seller_id,    null: false, foreign_key: true
       t.integer :buyer_id,     null: false, foreign_key: true
 
