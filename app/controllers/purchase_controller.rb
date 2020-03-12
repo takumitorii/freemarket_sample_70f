1c51b3e9818e9041b7e4d3656b42eea70b908fb5
takumitorii <takumitorii@users.noreply.github.com>
2020-03-12 18:37:29 +0900

    あ

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -16,7 +16,7 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 13500,
+    :amount => 13500, #仮置きです、productのpriceを表示します
     :customer => card.customer_id,
     :currency => 'jpy',
   )
