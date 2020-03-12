08a247ca2def619fe10446508141f5a7644a5715
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-12 17:09:10 +0900

    Update shipping.rb

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,4 +1,8 @@
 class Shipping < ApplicationRecord
   has_many :products
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
 end
