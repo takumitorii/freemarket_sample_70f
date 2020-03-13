f48d6e8fb37bc5fb4ba23f7555bf7dc61cc197f3
mayuken <superbeaver.drum@gmail.com>
2020-03-13 16:33:47 +0900

    JS_fix
    カテゴリのプルダウン挙動修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,12 +1,13 @@
-window.addEventListener('DOMContentLoaded', function(){
+$(function(){
+  console.log("ok")
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category){
-    var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    let html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
     return html;
   }
   // 子カテゴリーの表示作成
   function appendChidrenBox(insertHTML){
-    var childSelectHtml = '';
+    let childSelectHtml = '';
     childSelectHtml = `<div class='selle_contents__content--category' id= 'children_wrapper'>
                         <select class="product_category" id="child_category" name="category">
                           <option value="---" data-category="---">---</option>
@@ -17,7 +18,7 @@ window.addEventListener('DOMContentLoaded', function(){
   }
   // 孫カテゴリーの表示作成
   function appendGrandchidrenBox(insertHTML){
-    var grandchildSelectHtml = '';
+    let grandchildSelectHtml = '';
     grandchildSelectHtml = `<div class='selle_contents__content--category' id= 'grandchildren_wrapper'>
                               <select class="product_category" id="grandchild_category" name="category">
                                 <option value="---" data-category="---">---</option>
@@ -28,7 +29,8 @@ window.addEventListener('DOMContentLoaded', function(){
   }
   // 親カテゴリー選択後のイベント
   $('#product_category').on('change', function(){
-    var parentCategory = document.getElementById('product_category').value; //選択された親カテゴリーの名前を取得
+    console.log("ok2")
+    let parentCategory = document.getElementById('product_category').value; //選択された親カテゴリーの名前を取得
     if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_children',
@@ -41,7 +43,7 @@ window.addEventListener('DOMContentLoaded', function(){
         $('#grandchildren_wrapper').remove();
         $('#size_wrapper').remove();
         $('#brand_wrapper').remove();
-        var insertHTML = '';
+        let insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
         });
@@ -59,7 +61,7 @@ window.addEventListener('DOMContentLoaded', function(){
   });
   // 子カテゴリー選択後のイベント
   $('.selle_contents__content--category').on('change', '#child_category', function(){
-    var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
+    let childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
     if (childId != "---"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_grandchildren',
