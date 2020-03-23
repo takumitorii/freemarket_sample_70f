5cc575f5c675a0bf468ab687ef5f40aa9d5e56dc
oriaki1231 <aki.raliver.1231@icloud.com>
2020-03-23 11:38:00 +0900

    update

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,5 @@
 class Destination < ApplicationRecord
   belongs_to :user
+
+  validates :family_name, :family_name_kana, :first_name, :first_name_kana, :post_code, :prefecture, :city, :address, presence: true
 end
